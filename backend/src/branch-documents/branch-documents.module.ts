5801ae4408eb3365ddbdd00baeaae29d5c574068
Ahmad Mubarak <Ahmad.Mubarak@sama.com.kw>
2026-07-13 11:15:05 +0300

    fix: import PrismaModule in BranchDocumentsModule and VisitorsModule (DI crash on boot)

--- backend/src/branch-documents/branch-documents.module.ts
+++ backend/src/branch-documents/branch-documents.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { BranchDocumentsController } from './branch-documents.controller';
 import { BranchDocumentsService } from './branch-documents.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [BranchDocumentsController],
   providers: [BranchDocumentsService],
   exports: [BranchDocumentsService],
--- backend/src/visitors/visitors.module.ts
+++ backend/src/visitors/visitors.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { VisitorsController } from './visitors.controller';
 import { VisitorsService } from './visitors.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [VisitorsController],
   providers: [VisitorsService],
   exports: [VisitorsService],
